4cea18d75b763920991cef3b984595ac77967530
Lindsay Qin <lindsaymqin@gmail.com>
2025-01-02 12:37:24 -0800

    Added animations.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,21 +14,21 @@ const Home = () => {
         <div className="content">
             <div className="home-page">
                 <div className="intro-text">
-                    <h1>Hi, I'm Lindsay!</h1>
-                    <h2>Welcome to my personal site.</h2>
+                    <h1 className="typed">Hi, I'm Lindsay!</h1>
+                    <h2 className="fadein frame2">Welcome to my personal site.</h2>
                     <br></br>
-                    <p>Right now, I'm a 2nd-year student at <a href="https://www.ucla.edu/" target="_blank" rel="noopener noreferrer">UCLA</a> majoring in Computer Science and minoring in Public Affairs.</p>
+                    <p className="fadein frame2">Right now, I'm a 2nd-year student at <a href="https://www.ucla.edu/" target="_blank" rel="noopener noreferrer">UCLA</a> majoring in Computer Science and minoring in Public Affairs.</p>
                     <br></br>
-                    <p>I'm also a</p>
-                    <ul>
+                    <p className="fadein frame2">I'm also a</p>
+                    <ul className="fadein frame2">
                         <li>previous intern at <a href="https://www6.slac.stanford.edu/" target="_blank" rel="noopener noreferrer">SLAC</a></li>
                         <li>undergrad researcher at UCLA's <a href="https://structures.computer/" target="_blank" rel="noopener noreferrer">SCI Lab</a></li>
                         <li>and a bad joke enthusiast!</li>
                     </ul>
                     <br></br>
-                    <p>While you're here, feel free to check out what I've been up to, from <Link to='/work'>professional projects</Link> to <Link to='/about'>personal passions</Link>.</p>
+                    <p className="fadein frame2">While you're here, feel free to check out what I've been up to, from <Link to='/work'>professional projects</Link> to <Link to='/about'>personal passions</Link>.</p>
                 </div>
-                <img src={headshot} alt="Lindsay Qin professional headshot."></img>
+                <img className="fadein" src={headshot} alt="Lindsay Qin professional headshot."></img>
             </div>
         </div>
         <Contact />
